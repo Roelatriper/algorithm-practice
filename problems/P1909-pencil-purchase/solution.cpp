d134d2e520f33e865d46b13d9da2537cc27b77c1
aresene <2791917890@qq.com>
2026-09-21 15:23:23 +0800

    搞定了半个phase

--- problems/P1909-pencil-purchase/solution.cpp
+++ problems/P1909-pencil-purchase/solution.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 int main(){
-    //blank,wait 4 input.
-
+    int n;
+    cin >> n;
+    int a[3],b[3],tmp[3],ans;
+    for(int i=0;i<3;i++){
+        cin >> a[i] >> b[i];
+        if(n%a[i]!=0){
+            int j=n%a[i];
+            n+=(a[i]-j);
+        }
+        tmp[i]=(n/a[i])*b[i];
+    }ans=tmp[0];
+    for(int i=1;i<=2;i++){
+        ans = min(ans, tmp[i]);
+    }
+    cout << ans << endl;
     return 0;
 }
