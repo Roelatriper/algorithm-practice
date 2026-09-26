d57ff06e18114bd73fe42ea2cf8bfc4f44ab34a0
aresene <2791917890@qq.com>
2026-09-26 17:47:00 +0800

    p1093在做，目前问题是遇上多个同样例时没有比较，直接跳过了。解决方法：排序时二次维护，不用使用输出时大量选择狗屎分支。同文件路径下有样例点.结论：煞笔。

---
+++ problems/P1093-scholarship/attempt.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+struct student{
+    int a,b,c;
+    int sum,num;
+    bool summing(){
+        sum=a+b+c;
+        return 1;
+    }
+};
+bool istrue1(const student& a,const student& b){
+    return a.sum > b.sum;
+}
+int main(){
+    int n;
+    cin >> n;
+    vector<student> pupil(n);
+    for(int i=0;i<n;i++){
+        cin >> pupil[i].a >> pupil[i].b >> pupil[i].c;
+        pupil[i].num=i;
+        pupil[i].summing();
+    }
+    sort(pupil.begin(),pupil.end(),istrue1);
+    int fin=5;
+    for(int i=0;i<fin;i++){
+        if(pupil[i].sum==pupil[i+1].sum){
+            if(pupil[i].a==pupil[i+1].a){
+                if(pupil[i].b==pupil[i+1].b){
+                    if(pupil[i].c==pupil[i+1].c){
+                        cout << pupil[i].num+1 << " " << pupil[i].sum << endl;i+=2;fin++;
+                    }else {cout << pupil[i+!(pupil[i].c > pupil[i].c)].num+1 << " " << pupil[i+!(pupil[i].c > pupil[i].c)].sum << endl;i+=2;fin++;}
+                }else {cout << pupil[i+!(pupil[i].b > pupil[i].b)].num+1 << " " << pupil[i+!(pupil[i].b > pupil[i].b)].sum << endl;i+=2;fin++;}
+            }else {cout << pupil[i+!(pupil[i].a > pupil[i].a)].num+1 << " " << pupil[i+!(pupil[i].a > pupil[i].a)].sum << endl;i+=2;fin++;}
+        }else {cout << pupil[i].num+1 << " " << pupil[i].sum << endl;}
+    }
+    return 0;
+}
