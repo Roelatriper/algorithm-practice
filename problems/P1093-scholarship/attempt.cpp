dd921e0a51137401174977f25a07460633ba7252
aresene <2791917890@qq.com>
2026-09-26 18:04:57 +0800

    糖完了。

--- problems/P1093-scholarship/attempt.cpp
+++ problems/P1093-scholarship/attempt.cpp
@@ -12,7 +12,7 @@ struct student{
     }
 };
 bool istrue1(const student& a,const student& b){
-    return a.sum > b.sum;
+    if(a.sum == b.sum){if(a.a == b.a){return a.num < b.num;}else return a.a > b.a;}else return a.sum > b.sum;
 }
 int main(){
     int n;
@@ -24,17 +24,19 @@ int main(){
         pupil[i].summing();
     }
     sort(pupil.begin(),pupil.end(),istrue1);
-    int fin=5;
-    for(int i=0;i<fin;i++){
-        if(pupil[i].sum==pupil[i+1].sum){
-            if(pupil[i].a==pupil[i+1].a){
-                if(pupil[i].b==pupil[i+1].b){
-                    if(pupil[i].c==pupil[i+1].c){
-                        cout << pupil[i].num+1 << " " << pupil[i].sum << endl;i+=2;fin++;
-                    }else {cout << pupil[i+!(pupil[i].c > pupil[i].c)].num+1 << " " << pupil[i+!(pupil[i].c > pupil[i].c)].sum << endl;i+=2;fin++;}
-                }else {cout << pupil[i+!(pupil[i].b > pupil[i].b)].num+1 << " " << pupil[i+!(pupil[i].b > pupil[i].b)].sum << endl;i+=2;fin++;}
-            }else {cout << pupil[i+!(pupil[i].a > pupil[i].a)].num+1 << " " << pupil[i+!(pupil[i].a > pupil[i].a)].sum << endl;i+=2;fin++;}
-        }else {cout << pupil[i].num+1 << " " << pupil[i].sum << endl;}
+    //cout << endl << "br" << endl;
+    for(int i=0;i<5;i++){
+        cout << pupil[i].num+1 << " " << pupil[i].sum << endl;
     }
     return 0;
 }
+
+// if(pupil[i].sum==pupil[i+1].sum){
+//             if(pupil[i].a==pupil[i+1].a){
+//                 if(pupil[i].b==pupil[i+1].b){
+//                     if(pupil[i].c==pupil[i+1].c){
+//                         cout << pupil[i].num+1 << " " << pupil[i].sum << endl;i+=2;fin++;
+//                     }else {cout << pupil[i+!(pupil[i].c > pupil[i].c)].num+1 << " " << pupil[i+!(pupil[i].c > pupil[i].c)].sum << endl;i+=2;fin++;}
+//                 }else {cout << pupil[i+!(pupil[i].b > pupil[i].b)].num+1 << " " << pupil[i+!(pupil[i].b > pupil[i].b)].sum << endl;i+=2;fin++;}
+//             }else {cout << pupil[i+!(pupil[i].a > pupil[i].a)].num+1 << " " << pupil[i+!(pupil[i].a > pupil[i].a)].sum << endl;i+=2;fin++;}
+//         }else {cout << pupil[i].num+1 << " " << pupil[i].sum << endl;}
